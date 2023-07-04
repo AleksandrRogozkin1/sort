df2e0cc739c17355eb0661e70b4cf0b05cba98c8
Aleksandr Rogozhkin <rogozkin371@gmail.com>
2023-07-04 15:09:56 +0300

    innit2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ import org.example.sortService.SortAlphabetically;
 import org.example.utils.FileReader;
 
 import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         List<String> res=new FileReader("text").reading();
